578a22c4191e9133b2c23cd4166e6de6fb9860c9
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2024-02-16 00:38:37 +0000

    feat(index): added linguistic properties to index. Added maximum length filter to KW search UI.

--- helpers/settings.py
+++ helpers/settings.py
@@ -17,21 +17,21 @@ DOMAINS = {
     },
     'history': {
         'name': 'History',
-        'path': 'eb07/XML/h11/kp-eb0711-046904-5192-v1.xml',
+        'path': 'eb07/XML_V2/h11/kp-eb0711-046904-5192-v2.xml',
         'name_modern': ['history', 'mythology', 'legend', 'religion', 'politics', 'war'],
         'fast': ['history'],
         'desc': 'Includes biblical history of the earth and growth of society under "Civil History". See also entries on countries, like RUSSIA--these are generally histories.',
     },
     'natural_philosophy': {
         'name': 'Natural Philosophy',
-        'path': 'eb07/XML/n15/kp-eb0715-074001-8829-v1.xml',
+        'path': 'eb07/XML_V2/n15/kp-eb0715-074001-8829-v2.xml',
         'name_modern': ['physics', 'mechanics', 'optics', 'astronomy', 'magnetism', 'electricity'],
         'fast': ['physics'],
         'desc': 'Similar to Physics. "NATURAL PHILOSOPHY is commonly defined to be that branch of knowledge which considers the powers and properties of natural bodies, and their mutual actions on one another. This term serves to indicate, not one, but a cluster of sciences. Those generally comprehended under it are the following, viz. 1. Mechanics; 2. Hydrostatics; 3. Optics; 4. Astronomy; 5. Magnetism; 6. Electricity." (kp-eb0715-8829-0740-01)',
     },
     'natural_history': {
         'name': 'Natural History',
-        'path': 'eb07/XML/n15/kp-eb0715-073804-8803-v1.xml',
+        'path': 'eb07/XML_V2/n15/kp-eb0715-073804-8803-v2.xml',
         'name_modern': ['meteorology', 'mineralogy', 'botany', 'zoology'],
         'fast': ['biology', 'life science', 'earth sciences', 'natural history'],
         'desc': 'that part of natural knowledge which teaches us to distinguish and describe the objects of nature; to examine their appearance, structure, properties, and uses; and to collect, preserve, and arrange them',
@@ -67,12 +67,12 @@ CUSTOM_SAMPLES = [
     },
     {
         'heading': 'HAMMER',
-        'path': 'eb07/XML/h11/kp-eb0711-012702-0746-v1.xml',
+        'path': 'eb07/XML_V2/h11/kp-eb0711-012702-0746-v2.xml',
         'domain': 'useful_arts'
     },
     {
         'heading': 'RUSSIA',
-        'path': 'eb07/XML/r19/kp-eb0719-052701-0539-v1.xml',
+        'path': 'eb07/XML_V2/r19/kp-eb0719-052701-0539-v2.xml',
         'domain': 'history'
     },
 ]
@@ -80,37 +80,37 @@ CUSTOM_SAMPLES = [
 CUSTOM_SAMPLES += [
     {
         'heading': 'NAMUR',
-        'path': 'eb07/XML/n15/kp-eb0715-068902-8166-v1.xml',
+        'path': 'eb07/XML_V2/n15/kp-eb0715-068902-8166-v2.xml',
         'domain': 'history'
     },
     {
         'heading': 'MILAN',
-        'path': 'eb07/XML/m15/kp-eb0715-006306-0028-v1.xml',
+        'path': 'eb07/XML_V2/m15/kp-eb0715-006306-0028-v2.xml',
         'domain': 'history'
     },
     {
         'heading': 'OEDIPUS',
-        'path': 'eb07/XML/o16/kp-eb0716-032407-1077-v1.xml',
+        'path': 'eb07/XML_V2/o16/kp-eb0716-032407-1077-v2.xml',
         'domain': 'history'
     },
     {
         'heading': 'PLAGUE',
-        'path': 'eb07/XML/p17/kp-eb0717-077207-1123-v1.xml',
+        'path': 'eb07/XML_V2/p17/kp-eb0717-077207-1123-v2.xml',
         'domain': 'medicine'
     },
     {
         'heading': 'SYMPHONY',
-        'path': 'eb07/XML/s21/kp-eb0721-005309-0071-v1.xml',
+        'path': 'eb07/XML_V2/s21/kp-eb0721-005309-0071-v2.xml',
         'domain': 'fine_arts'
     },
     {
         'heading': 'REFRACTION',
-        'path': 'eb07/XML/r19/kp-eb0719-009701-0107-v1.xml',
+        'path': 'eb07/XML_V2/r19/kp-eb0719-009701-0107-v2.xml',
         'domain': 'natural_philosophy'
     },
     {
         'heading': 'FRICTION',
-        'path': 'eb07/XML/f10/kp-eb0710-023209-6983-v1.xml',
+        'path': 'eb07/XML_V2/f10/kp-eb0710-023209-6983-v2.xml',
         'domain': 'natural_philosophy'
     }
 ]
